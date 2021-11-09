b940e1bc2fbff626343dba42b8faaaf7c7d6bc97
Alexander Britcliffe <alexanderbritcliffe@gmail.com>
2021-11-09 11:50:02 -0500

    added link block

--- seleniumexamples/HandlingDropdowns.py
+++ seleniumexamples/HandlingDropdowns.py
@@ -19,9 +19,22 @@ select = Select(dropdown)
 
 select.select_by_value("hi")
 
-options = driver.find_elements_by_tag_name("option")
+options = dropdown.find_elements_by_tag_name("option")
 
 for option in options:
     print("Text is : ",option.text, "Lang is : "+option.get_attribute("lang"))
 
 print("Total dropdown values are ", len(options))
+
+print("-----Printing all links-------")
+
+block = driver.find_element_by_xpath("//*[@id=\"www-wikipedia-org\"]/div[11]/div[3]")
+
+links = block.find_elements_by_tag_name("a")
+
+print("1st link is : ",block.find_elements_by_tag_name("a").__getitem__(0).text)
+
+print(len(links))
+
+for links in links:
+    print("Text is : ",link.text," ---URL is : "+link.get_attribute("href"))
