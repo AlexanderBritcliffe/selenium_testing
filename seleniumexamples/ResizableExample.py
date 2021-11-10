455298fce3cb8b61f62bc4f01b03d5fae9fb92e7
Alexander Britcliffe <alexanderbritcliffe@gmail.com>
2021-11-10 14:36:32 -0500

    added action chains for mouse movement sliders and resize

---
+++ seleniumexamples/ResizableExample.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(executable_path=ChromeDriverManager().install)
+
+driver.get("https://jqueryui.com/resources/demos/resizable/default.html")
+driver.maximize_window()
+driver.implicity_wait(1)
+
+resizable = driver.find_elements_by_xpath("//*[@id=\"resizable\"]/div[3]")
+
+ActionChains(driver).drag_and_drop_by_offset(resizable,400,400).perform()
